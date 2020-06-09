9c9ed88258eff31199c626ec801a44b9ddb1cbb3
fgarciah16 <fgarciahernandez2@gmail.com>
2020-06-09 11:22:30 -0500

    Comentado

--- gato.c
+++ gato.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 	
-	char matriz[3][3], opc;
+	char matriz[3][3], opc; // Se declara la matriz de tres por tres 
 	int i, j;
 	
 	printf("Juego del gato!\n");
@@ -9,35 +9,36 @@ int main(){
 	
 	for(i=0; i<3; i++){
 		for(j=0; j<3; j++){
-			matriz[i][j]=' ';
+			matriz[i][j]=' ';     // Se crea la matriz
 			printf("[%c]", matriz[i][j]);
 		}
 		printf("\n");
 	}
 	
-	printf("\n\nPresione Y para jugar / N para salir: ");
+	printf("\n\nPresione Y para jugar / N para salir: ");  //Opcion para salir o jugar 
 	scanf("%c", &opc);
 	
-	int fila, col, ganador=0, turno=1;
+	int fila, col, ganador=0, turno=1;     // Turno UNO 
 	
-	if(opc == 'Y' || opc == 'y'){
+	if(opc == 'Y' || opc == 'y'){ // Seleccion para jugar
 		
+		//Turno para el jugador uno 
 		do{
 			if(turno%2==1){
 				do{
 					printf("\nJugador 1: \n");
 					printf("Digite fila: ");
-					scanf("%d", &fila);
+					scanf("%d", &fila);             
 					printf("Digite columna: ");
 					scanf("%d", &col);
-					
+					// Coordenadas para que no haya repeticiones en la matriz
 					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
 						printf("\nCoordenadas no validas, pruebe otra vez.\n");
-					}
+					}                               
 				}while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
 				
-				matriz[fila][col]='x';
-				
+				matriz[fila][col]='x'; // S ellena la matriz del jugador 1 con x
+			    //Limpa pantalla
 				system("cls");
 				for(i=0; i<3; i++){
 					for(j=0; j<3; j++){
@@ -45,21 +46,27 @@ int main(){
 					}
 					printf("\n");
 				}
-				turno++;
-			} else if(turno%2==0){
+				turno++; // Contador del turno 
+
+
+
+
+
+
+			} else if(turno%2==0){       //Turno del segundo jugador 
 				do{
 					printf("\nJugador 2: \n");
-					printf("Digite fila: ");
+					printf("Digite la fila: ");
 					scanf("%d", &fila);
-					printf("Digite columna: ");
+					printf("Digite la columna: ");
 					scanf("%d", &col);
-					
+					// Coordenadas para que no haya repeticiones en la matriz
 					if(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2){
 						printf("\nCoordenadas no validas, pruebe otra vez.\n");
 					}
 				} while(matriz[fila][col] == 'x' || matriz[fila][col] == 'o' || fila > 2 || col > 2);
 				
-				matriz[fila][col]='o';
+				matriz[fila][col]='o'; // Se llena la matriz del jugador 2 con circulos
 				
 				system("cls");
 				for(i=0; i<3; i++){
@@ -68,9 +75,12 @@ int main(){
 					}
 					printf("\n");
 				}
-				turno++;
+				turno++; // Contador para el turno 2
 			}
-			
+
+
+
+			//  Comprobaciones para primer jugador
 			if(matriz[0][0] == 'x' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
 			|| matriz[1][0] == 'x' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
 			|| matriz[2][0] == 'x' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
@@ -81,10 +91,10 @@ int main(){
 			
 			|| matriz[0][0] == 'x' && matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]
 			|| matriz[0][2] == 'x' && matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0]){
-				ganador=1;
+				ganador=1;  // ganador
 				printf("\nFelicidades! Gano el jugador 1.\n");
 			}
-			
+			// Comprobaciones para segundo jugador 
 			if(matriz[0][0] == 'o' && matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2]
 			|| matriz[1][0] == 'o' && matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2]
 			|| matriz[2][0] == 'o' && matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2]
@@ -101,8 +111,8 @@ int main(){
 			
 		} while(ganador != 1);
 	} else {
-		system("cls");
-		printf("\nDe todas formas no queria que jugaras.\n");
+		system("cls"); //Limpia pantalla
+		printf("\n Que lastima! Que tengas buen dia.\n");
 	}
 	
 	return 0;
